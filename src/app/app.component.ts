7a8a05e2a8d94854b446df5592531e759bf19467
astritdemiri11 <astritdemiri11@gmail.com>
2022-09-11 21:29:03 +0200

    First test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngx-arrangement';
+export class AppComponent implements AfterViewInit {
+  private alerted: boolean;
+
+  constructor() {
+    this.alerted = false;
+  }
+
+  @ViewChild('content', { static: false }) content?: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit() {
+    fromEvent(window, 'scroll').subscribe(() => {
+      if (!this.alerted && this.content) {
+        this.alerted = true;
+        alert('Content Loaded');
+      }
+    });
+  }
 }
